73030cb5fab91dc26e7f1df1f64047cc288d3719
Taras Shypka <tarashypka@gmail.com>
2019-04-29 12:38:53 +0300

    parallel: Updated map_reduce()

--- pysimple/parallel.py
+++ pysimple/parallel.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 from functools import partial
 from typing import *
+
 from tqdm import tqdm
 
 
@@ -13,30 +14,41 @@ def _map_func_wrapper(*args, map_func: Callable, que: mp.Queue, **kwargs):
     return res
 
 
-def _tqdm_listener(que: mp.Queue, total: int):
-    progress_bar = tqdm(total=total)
+def _tqdm_listener(que: mp.Queue, total: int, bar=tqdm):
+    progress_bar = bar(total=total)
     for _ in iter(que.get, None):
         progress_bar.update()
 
 
 def map_reduce(
-        inputs: List, workers: int, map_func: Callable, reduce_func: Callable=None,
-        reduce_init=None, **map_func_kwargs):
+        inputs: List, workers: int, map_func: Callable, reduce_func: Callable=None, reduce_init=None, progress_bar=tqdm,
+        **map_func_kwargs) -> "output of reduce_func or list of map_func outputs":
+    """Standard map-reduce routine to parallellize inputs between workers"""
 
-    que = mp.Manager().Queue()
-    tqdm_proc = mp.Process(target=_tqdm_listener, args=(que, len(inputs)))
-    tqdm_proc.start()
-
-    map_func = partial(_map_func_wrapper, map_func=map_func, que=que, **map_func_kwargs)
-
-    with mp.Pool(workers) as p:
-        if isinstance(inputs[0], tuple):
-            outputs = p.starmap(map_func, inputs)
+    if workers > 1:
+        que: mp.Queue = None
+        tqdm_proc: mp.Process = None
+        if progress_bar:
+            que = mp.Manager().Queue()
+            tqdm_proc = mp.Process(target=_tqdm_listener, args=(que, len(inputs), progress_bar))
+            tqdm_proc.start()
+            map_func = partial(_map_func_wrapper, map_func=map_func, que=que, **map_func_kwargs)
         else:
-            outputs = p.starmap(map_func, ((inp,) for inp in inputs))
+            map_func = partial(map_func, **map_func_kwargs)
 
-    que.put(None)
-    tqdm_proc.join()
+        with mp.Pool(workers) as p:
+            if isinstance(inputs[0], tuple):
+                outputs = p.starmap(map_func, inputs)
+            else:
+                outputs = p.starmap(map_func, ((inp,) for inp in inputs))
+
+        if progress_bar:
+            que.put(None)
+            tqdm_proc.join()
+    else:
+        if progress_bar:
+            inputs = progress_bar(inputs)
+        outputs = [map_func(inp, **map_func_kwargs) for inp in inputs]
 
     if reduce_func is None:
         return outputs
@@ -45,5 +57,3 @@ def map_reduce(
         for outp in outputs[1:]:
             reduced_outp = reduce_func(outp, reduced_outp)
         return reduced_outp
-
-
