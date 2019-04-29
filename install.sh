b447917eb75d0ec599da9c6e23d6fa27e358d46b
Taras Shypka <tarashypka@gmail.com>
2019-04-29 18:26:59 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ENV_NAME=$1
 MODULE_DIR=$(dirname $0)
 PYTHON_VERSION=3.7
+ANACONDA_PATH=/usr/local/anaconda
 
-cd ${MODULE_DIR}
+export PATH=${ANACONDA_PATH}/bin:$PATH
 
-ANACONDA=/usr/local/anaconda
-export PATH=${ANACONDA}/bin:$PATH
 if [[ ${ENV_NAME} == "" ]]; then
-    ENV_PATH=${ANACONDA}
+    ENV_PATH=${ANACONDA_PATH}
 else
-    ENV_PATH=${ANACONDA}/envs/${ENV_NAME}
-fi
-if [[ ! -d ${ENV_PATH} ]]; then
-    conda create -n ${ENV_NAME} python=${PYTHON_VERSION}
+    ENV_PATH=${ANACONDA_PATH}/envs/${ENV_NAME}
+    if [[ ! -d ${ENV_PATH} ]]; then
+        conda create -n ${ENV_NAME} python=${PYTHON_VERSION}
+    fi
+    source ${ANACONDA_PATH}/etc/profile.d/conda.sh
+    conda activate ${ENV_NAME}
 fi
 
-PIP=${ENV_PATH}/bin/pip
-${PIP} install -r requirements.txt
-${PIP} install .
+${ENV_PATH}/bin/pip install -r ${MODULE_DIR}/requirements.txt
+
+${ENV_PATH}/bin/python -m unittest discover -s ${MODULE_DIR}/tests
+
+${ENV_PATH}/bin/pip install ${MODULE_DIR}
