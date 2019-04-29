fa2e5b216235c4855ebf9df5375b5a3ade85ed06
Taras Shypka <tarashypka@gmail.com>
2019-04-29 13:17:40 +0300

    Updated text module

--- pysimple/text.py
+++ pysimple/text.py
@@ -1,4 +1,10 @@
+import pandas as pd
 
 
-def flatten_text(text: str) -> str:
-    return text.replace(r'\s+', ' ')
+def flatten_text(t: pd.Series, fillna: str='') -> pd.Series:
+    """Replace whitespace characters in text column with spaces"""
+    return t \
+        .fillna(fillna).astype(str) \
+        .str.strip() \
+        .str.rstrip() \
+        .str.replace(r'\s+', ' ')
