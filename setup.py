8e10d8297d8f1731ad5982d61f612cdca3c98875
Taras Shypka <tarashypka@gmail.com>
2020-03-28 17:49:04 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysimple',
-    version='1.0.0',
+    version='1.0.1',
     description='Utils functions to be used in other projects',
     url='',
     author='Taras Shypka',
     author_email='tarashypka@gmail.com',
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False)
